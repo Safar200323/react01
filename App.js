1a9999d3512258e7b7bfc1b803f217c1ff11cdfe
Safar200323 <142076988+Safar200323@users.noreply.github.com>
2023-09-10 18:45:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,69 +1,31 @@
-import React, { useState } from "react";
+import React, { Component } from 'react';
+import './App.css';
 
-// Importing app.css is css file to add styling
-import "./App.css";
+class App extends Component {
+  state = {
+    count: 0,
+  };
 
-const App = () => {
-// Counter is a state initialized to 0
-const [counter, setCounter] = useState(0)
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-// Function is called everytime increment button is clicked
-const handleClick1 = () => {
-	// Counter state is incremented
-	setCounter(counter + 1)
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <div className="counter">
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-// Function is called everytime decrement button is clicked
-const handleClick2 = () => {
-	// Counter state is decremented
-	setCounter(counter - 1)
-}
-
-return (
-	<div style={{
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	justifyContent: 'center',
-	fontSize: '300%',
-	position: 'absolute',
-	width: '100%',
-	height: '100%',
-	top: '-15%',
-	}}>
-	Counter App
-	<div style={{
-		fontSize: '120%',
-		position: 'relative',
-		top: '10vh',
-	}}>
-		{counter}
-	</div>
-	<div className="buttons">
-		<button style={{
-		fontSize: '60%',
-		position: 'relative',
-		top: '20vh',
-		marginRight: '5px',
-		backgroundColor: 'green',
-		borderRadius: '8%',
-		color: 'white',
-		}}
-		onClick={handleClick1}>Increment</button>
-		<button style={{
-		fontSize: '60%',
-		position: 'relative',
-		top: '20vh',
-		marginLeft: '5px',
-		backgroundColor: 'red',
-		borderRadius: '8%',
-		color: 'white',
-		}}
-		onClick={handleClick2}>Decrement</button>
-	</div>
-	</div>
-)
-}
-
-export default App
-
+export default App;
